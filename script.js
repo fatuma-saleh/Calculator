55c9e8c50bc1812a1ad7a494021199bd96ddda23
Fatuma <sfatex@gmail.com>
2022-04-22 12:56:44 -0400

    Implement compute and clear functions

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ class Calculator {
     this.clear();
   }
   clear() {
-    this.currentOperand = " ";
-    this.previousOperand = " ";
+    this.currentOperand = "";
+    this.previousOperand = "";
     this.operation = undefined;
   }
   delete() {}
@@ -15,13 +15,42 @@ class Calculator {
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
   chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.currentOperand !== "") {
+      this.compute();
+    }
     this.operation = operation;
     this.previousOperand = this.currentOperand;
-    this.currentOperand = " ";
+    this.currentOperand = "";
   }
-  compute() {}
+  compute() {
+    let result;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        result = prev + current;
+        break;
+      case "-":
+        result = prev - current;
+        break;
+      case "*":
+        result = prev * current;
+        break;
+      case "/":
+        result = prev / current;
+        break;
+      default :
+      return 
+    }
+    this.currentOperand = result
+    this.operation = undefined
+    this.previousOperand = ""
+  }
   updateDisplay() {
     this.currentTextElement.innerText = this.currentOperand;
+    this.previousTextElement.innerText = this.previousOperand;
   }
 }
 
@@ -46,4 +75,14 @@ operationButtons.forEach((button) => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+equalsButtons.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+allClearButtons.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
 });
