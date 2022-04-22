ed09aedfc30c57d060cda39ff7548f6a7043a1f2
Fatuma <sfatex@gmail.com>
2022-04-22 01:14:17 -0400

    Select all calculator elements using query selector

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll("[data-operation]")
+const equalsButtons = document.querySelector("[data-equals]")
+const deleteButtons = document.querySelector("[data-delete]")
+const previousTextElement = document.querySelector("[data-previous-operand]")
+const currentTextElement = document.querySelector("[data-current-operand]")
+const allClearButtons = document.querySelector("[data-all-clear]")
